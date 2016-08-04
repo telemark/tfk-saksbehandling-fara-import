bab925ac137e321a5a72a9cec2b67024c43dc4d9
maccyber <jonas.enge@gmail.com>
2016-08-05 01:56:06 +0200

    Various fixed

--- config/index.js
+++ config/index.js
@@ -2,11 +2,9 @@
 
 module.exports = {
   JWT_KEY: process.env.TFK_SFI_JWT_KEY || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
-  CALLBACK_STATUS_MESSAGE: process.env.TFK_SFI_CALLBACK_STATUS_MESSAGE || 'Søknad importert',
-  JOB_DIRECTORY_PATH: process.env.TFK_SFI_JOB_DIRECTORY_PATH || 'test/data/fara',
-  DISTRIBUTION_DIRECTORY_PATH: process.env.TFK_SFI_DISTRIBUTION_DIRECTORY_PATH || 'test/data/distribution',
-  ARCHIVE_DIRECTORY_PATH: process.env.TFK_SFI_ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
+  CALLBACK_STATUS_MESSAGE: process.env.TFK_SFI_CALLBACK_STATUS_MESSAGE || 'Importert i fara',
+  JOB_DIRECTORY_PATH: process.env.TFK_SFI_JOB_DIRECTORY_PATH || 'test/data/fara/jobs',
   DONE_DIRECTORY_PATH: process.env.TFK_SFI_DONE_DIRECTORY_PATH || 'test/data/fara/done',
-  ERROR_DIRECTORY_PATH: process.env.TFK_SFI_ERROR_DIRECTORY_PATH || 'test/data/errors',
+  ERROR_DIRECTORY_PATH: process.env.TFK_SFI_ERROR_DIRECTORY_PATH || 'test/data/fara/errors',
   API_URL: process.env.TFK_SFI_API_URL || 'https://api.skoleskyss.t-fk.no/applications'
 }
